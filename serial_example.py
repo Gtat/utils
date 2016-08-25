ad3ffd8472d33301a9b96e72a6f1a548ab5eec81
gtatyous <gtatyous>
2016-08-25 16:11:04 -0400

    8msec delay is required between two commands... MFC fully functioning

--- serial_example.py
+++ serial_example.py
@@ -3,23 +3,24 @@ if __name__ == '__main__':
   import serial
   import threading
   import sys
-
+  import time
+  
   print("sys default encoding: ", sys.getdefaultencoding())
 
   def reading(serial_port):
       print('reading rn')
       sys.stdout.flush()
-      raw_data = serial_port.read(size=60)
+      raw_data = serial_port.read(size=98)
       print ("raw data: ", raw_data)
       print()
       sys.stdout.flush()
-      print ("decoded: ", raw_data.strip().decode()) #UnicodeDecodeError
-      sys.stdout.flush()
+      #print ("decoded: ", raw_data.strip().decode()) #UnicodeDecodeError
+      #sys.stdout.flush()
 
   try:
-    ser = serial.Serial("COM14", 9600, timeout=3) #read will block since timeout=None
+    ser = serial.Serial("COM16", 9600, timeout=5) #read will block since timeout=None
 
-  except serial.serialutil.SerialException:
+  except serial.SerialException:
     print('exception raised')
 
   else:
@@ -35,7 +36,13 @@ if __name__ == '__main__':
     #print("new line \n")
     #sys.stdout.flush()
 
-    print("nuber of bytes written: ", ser.write('*A\r\n'.encode()))
+    print("number of bytes written: ", ser.write('*@=B\r\n'.encode()))
+    #print("number of bytes written: ", ser.write('*@=@\r\n'.encode()))
+    #print("number of bytes written: ", ser.write('C\r\n'.encode()))
+    time.sleep(.1)
+    print("number of bytes written: ", ser.write('B\r\n'.encode()))
+
+
     print('waiting on read thread')
     sys.stdout.flush()
     thread.join()
