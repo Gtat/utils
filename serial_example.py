ee2725ce1ce8a0ac624d0ad49346fb4f7d008baf
gtatyous <gtatyous>
2016-11-02 11:59:17 -0400

    serial tool for inst control

--- serial_example.py
+++ serial_example.py
@@ -1,53 +1,74 @@
 if __name__ == '__main__':
 
   import serial
+  import serial.tools.list_ports
   import threading
   import sys
   import time
-  
-  print("sys default encoding: ", sys.getdefaultencoding())
 
-  def reading(serial_port):
+
+  def reading(serial_port, stop_event, size):
       print('reading rn')
       sys.stdout.flush()
-      raw_data = serial_port.read(size=98)
-      print ("raw data: ", raw_data)
-      print()
-      sys.stdout.flush()
-      #print ("decoded: ", raw_data.strip().decode()) #UnicodeDecodeError
-      #sys.stdout.flush()
+      while(not stop_event.is_set()):
+        raw_data = serial_port.read(size=size)
+        print()
+        print ("raw data: ", raw_data)
+        sys.stdout.flush()
+        print ("decoded: ", raw_data.strip().decode()) #UnicodeDecodeError
+        sys.stdout.flush()
+
+
+  print("sys default encoding: ", sys.getdefaultencoding())
+  devs = serial.tools.list_ports.comports()
+
+  print("Choose from the connecting ports")
+  for dev in devs:
+      print("\t", dev.device)
+
+  com = str(input("which port do you want to open> "))
+  baudrate = int(input("what is the baudrate> "))
+  read_timeout = int(input("what is read timeout (input 0 to wait forever)> "))
+  if (read_timeout == 0):
+      read_timeout = None
+  print("openning ", com, "with baudrate ", baudrate, " and read timouet=", read_timeout)
 
   try:
-    ser = serial.Serial("COM16", 9600, timeout=5) #read will block since timeout=None
-
+    ser = serial.Serial(com, baudrate, timeout=read_timeout) #read will block if timeout=None
   except serial.SerialException:
     print('exception raised')
-
   else:
     print(ser.name)
     if ser.isOpen():
       print('serial is open!')
 
-    thread = threading.Thread(target=reading, args=(ser,))
+    size = int(input("what is the read buffer size in bytes> "))
+    thread_stop = threading.Event()
+    thread = threading.Thread(target=reading, args=(ser, thread_stop, size))
     thread.start()
 
-    #print("heallo".encode('utf-8'))
-    #print('@=B'.encode('utf-8'))
-    #print("new line \n")
-    #sys.stdout.flush()
 
-    print("number of bytes written: ", ser.write('*@=B\r\n'.encode()))
-    #print("number of bytes written: ", ser.write('*@=@\r\n'.encode()))
-    #print("number of bytes written: ", ser.write('C\r\n'.encode()))
-    time.sleep(.1)
-    print("number of bytes written: ", ser.write('B\r\n'.encode()))
+    while(1):
+
+        query = str(input("type your query> "))
+        wr_term = int(input("write terminator: input 0 for CR and 1 for CRLF> "))
+        if (wr_term):
+            wr_term = '\r\n'
+        else:
+            wr_term = '\r'
+        if (query == "exit"):
+            thread_stop.set()
+            break
+        else:
+            print("number of bytes written: ", ser.write((query + wr_term).encode()))
 
 
     print('waiting on read thread')
     sys.stdout.flush()
     thread.join()
 
-    print("serial closed")
+    print("serial is closing")
+    sys.stdout.flush()
     ser.close()
 
 
